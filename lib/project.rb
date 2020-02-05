45e0a020026e3a0bbd88300204247cf6fa1a703b
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-05 22:05:15 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Project
+  attr_reader :title, :backers
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    if backer.instance_of?
+
+
+end 
